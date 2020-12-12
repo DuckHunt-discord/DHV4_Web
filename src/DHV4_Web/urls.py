7dacd12b5662ca2f81dbaa6c0a2b0bb79af1d97a
Arthur Jovart <arthur@jovart.com>
2020-12-12 02:29:43 +0100

    Fix used URLs

--- src/DHV4_Web/urls.py
+++ src/DHV4_Web/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('public.urls')),
-    path('/data/', include('botdata.urls'))
+    path('data/', include('botdata.urls'))
 ]
