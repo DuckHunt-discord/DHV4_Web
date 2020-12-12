96f513486f7bbf71c6d3d6c689a288d90e43ff53
Arthur Jovart <arthur@jovart.com>
2020-12-12 02:33:15 +0100

    Register admin models

--- src/botdata/admin.py
+++ src/botdata/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from . import models
 # Register your models here.
+
+admin.register(models.Aerich)
+admin.register(models.Channels)
+admin.register(models.Guilds)
+admin.register(models.Members)
+admin.register(models.Players)
+admin.register(models.Users)
