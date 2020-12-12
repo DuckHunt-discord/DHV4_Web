c034689510d7ae0801679a7a92586abb3abd98ac
Arthur Jovart <arthur@jovart.com>
2020-12-12 02:46:08 +0100

    Remove Aerich from Admin panel

--- src/botdata/admin.py
+++ src/botdata/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Aerich)
+#admin.site.register(models.Aerich)
 admin.site.register(models.Channels)
 admin.site.register(models.Guilds)
 admin.site.register(models.Members)
