a19cd3f031deb5137abce07e919a91a91770dc71
Arthur Jovart <arthur@jovart.com>
2020-12-14 11:32:27 +0100

    Try to capture django output

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic --clear --no-input
 python manage.py migrate --no-input
 
-exec gunicorn DHV4_Web.wsgi:application --access-logfile '-' --error-logfile '-' --log-level "debug" --bind 0.0.0.0:8080 --workers=4
+exec gunicorn DHV4_Web.wsgi:application --access-logfile '-' --error-logfile '-' --log-level "debug" --capture-output True --bind 0.0.0.0:8080 --workers=4
