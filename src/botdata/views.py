c5d477615dbfb3b2a472d5f452c7962da003b787
Arthur Jovart <arthur@jovart.com>
2020-12-17 12:12:16 +0100

    Update colors used

--- src/botdata/views.py
+++ src/botdata/views.py
@@ -1,20 +1,68 @@
 import random
 
-from django.db.models import Prefetch, Q, Count
+from django.core.paginator import Paginator, Page
+from django.db.models import Prefetch, Q, Count, Exists, OuterRef, Max
 from django.shortcuts import render, get_object_or_404
 from .models import DiscordGuild, DiscordChannel, DiscordUser, Player
 
+
 # Create your views here.
 
-def guilds(request):
-    enabled_channels_qs = DiscordChannel.objects.filter(enabled=True)
-    guilds_list = DiscordGuild.objects \
-        .annotate(enabled_channels_count=Count('channels', filter=Q(channels__enabled=True))) \
-        .filter(enabled_channels_count__gt=0) \
-        .prefetch_related(Prefetch("channels", queryset=enabled_channels_qs.annotate(player_count=Count("players")))) \
-        .order_by('vip', 'pk')
+class CustomPage(Page):
+    def included_range(self, start, stop, step=1):
+        return range(max(0, start), min(self.paginator.num_pages, stop), step)
 
-    return render(request, "botdata/guilds.jinja2", {"guilds": guilds_list})
+    def get_pages_to_show(self):
+        shown_numbers = []
+
+        current_page = self.number
+
+        round_offset = current_page % 10
+
+        shown_numbers.extend(self.included_range(current_page-(5+round_offset), current_page+(10-round_offset))[:10])
+        shown_numbers.extend(self.included_range(current_page-(50+round_offset), current_page+(100-round_offset), 10)[:10])
+        shown_numbers.extend(self.included_range(current_page-(500+round_offset), current_page+(1000-round_offset), 100)[:10])
+
+        shown_numbers = list(set(shown_numbers))
+
+        shown_numbers.sort()
+
+        if shown_numbers[0] == 0:
+            shown_numbers = shown_numbers[1:]
+
+        return shown_numbers
+
+
+class CustomPaginator(Paginator):
+    def _get_page(self, *args, **kwargs):
+        """
+        Return an instance of a single page.
+
+        This hook can be used by subclasses to use an alternative to the
+        standard :cls:`Page` object.
+        """
+        return CustomPage(*args, **kwargs)
+
+
+def guilds(request):
+    enabled_channels_qs = DiscordChannel.objects \
+        .filter(enabled=True) \
+        .annotate(player_count=Count("players")) \
+        .filter(player_count__gt=0)
+
+    guilds_list = DiscordGuild.objects \
+        .filter(Q(channels__in=enabled_channels_qs) & Q(channels__isnull=False)) \
+        .prefetch_related(
+            Prefetch("channels", queryset=enabled_channels_qs)
+        ) \
+        .distinct() \
+        .order_by('-vip')
+
+    guilds_paginator = CustomPaginator(guilds_list, 50)
+    page_number = request.GET.get('page', 1)
+    page_obj = guilds_paginator.get_page(page_number)
+
+    return render(request, "botdata/guilds.jinja2", {"guilds": page_obj})
 
 
 def guild(request, pk: int):
