9b4ca3dc8997410a3ffdc9ca0a4da6435ca52c9b
Arthur Jovart <arthur@jovart.com>
2020-12-29 14:02:29 +0100

    Simplified iteration

--- src/docs/md_processors/remove_md_links_extension.py
+++ src/docs/md_processors/remove_md_links_extension.py
@@ -2,20 +2,12 @@ from markdown.treeprocessors import Treeprocessor
 from markdown.extensions import Extension
 
 
-def get_all_element_children(root):
-    childs = []
-    for child in root:
-        childs.append(child)
-        childs.extend(get_all_element_children(child))
-    return childs
-
 class MdLinksTreeprocessor(Treeprocessor):
     def run(self, root):
-        for element in get_all_element_children(root):
-            if element.tag == 'a':
-                href = element.get('href')
-                if href.endswith('.md'):
-                    element.set('href', href[:-3])
+        for element in root.iter('a'):
+            href = element.get('href')
+            if href.endswith('.md'):
+                element.set('href', href[:-3])
 
         # No return statement is same as `return None`
 
