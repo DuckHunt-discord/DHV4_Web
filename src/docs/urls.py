43717ae7f92b9984bbf8dac6157e0f7fd9bac7ce
Arthur Jovart <arthur@jovart.com>
2020-12-31 19:04:02 +0100

    Add meta descriptions and fix titles

--- src/docs/urls.py
+++ src/docs/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('README', views.index_redirect,),
     path('', views.display_page, name="index", kwargs={"path": "README"}),
     path('summary', views.summary, name="summary"),
+    path('SUMMARY', views.summary, name="summary"),
+    path('SUMMARY/', views.summary, name="summary"),
+    path('INDEX', views.summary, name="summary"),
     path('.gitbook/assets/<str:file>', views.assets, name="assets"),
     re_path(r'(?P<path>(?:[A-z0-9\-]*/?)*)', views.display_page, name="page"),
 ]
