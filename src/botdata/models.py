9f98aac990986d1fd8723302e1c02979e0d9d140
Arthur Jovart <arthur@jovart.com>
2021-02-16 00:13:34 +0100

    Add a verified field and improve the form to add botlists.

--- src/botdata/models.py
+++ src/botdata/models.py
@@ -404,6 +404,9 @@ class BotList(models.Model):
 
     # **Others**
 
+    bot_verified = models.BooleanField(help_text="Whether the bot was verified by the bot list staff",
+                                       default=False)
+
     bot_certified = models.BooleanField(help_text="Whether the bot was certified on that bot list",
                                         default=False)
 
@@ -411,7 +414,7 @@ class BotList(models.Model):
                                   blank=True)
 
     def __str__(self):
-        return f"<BotList {self.name}>"
+        return f"BotList {self.name}"
 
     class Meta:
         db_table = 'botlist'
