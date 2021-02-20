b18df3141a0d64bee58709374d67112f13666809
Arthur Jovart <arthur@jovart.com>
2021-02-20 21:43:38 +0100

    Add tags models and admin data

--- src/tags/admin.py
+++ src/tags/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from dynamic_raw_id.admin import DynamicRawIDMixin
+
+from . import models
+
 
 # Register your models here.
+
+class AliasesInline(DynamicRawIDMixin, admin.TabularInline):
+    dynamic_raw_id_fields = ["owner"]
+
+    model = models.TagAliases
+
+
+class TagsAdmin(DynamicRawIDMixin, admin.ModelAdmin):
+    dynamic_raw_id_fields = ["owners"]
+    inlines = [
+        AliasesInline,
+    ]
+
+
+admin.site.register(models.Tags, TagsAdmin)
+
--- src/tags/models.py
+++ src/tags/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tags(models.Model):
+    owners = models.ManyToManyField('botdata.DiscordUser', related_name='tags')
+
+    # Statistics
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    uses = models.IntegerField(default=0)
+    revisions = models.IntegerField(default=0)
+
+    # Tag
+    official = models.BooleanField(default=False)
+    name = models.CharField(max_length=90, db_index=True)
+    content = models.TextField()
+
+
+class TagAliases(models.Model):
+    owner = models.ForeignKey('botdata.DiscordUser', related_name='tags_aliases', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tags, related_name='aliases', on_delete=models.CASCADE)
+
+    uses = models.IntegerField(default=0)
+
+    name = models.CharField(max_length=90, db_index=True)
+
