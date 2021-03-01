c7ff75eb821ddd89264d4f2c9a8622472cfc5d7e
Arthur Jovart <arthur@jovart.com>
2021-03-01 17:53:40 +0100

    Filter elements in the menu

--- src/shop/templatetags/global_jinja_funcs.py
+++ src/shop/templatetags/global_jinja_funcs.py
@@ -10,7 +10,7 @@ def get_shop_product_types():
     product_types_infos = cache.get('shop_product_type_infos', [])
 
     if not product_types_infos:
-        for product_type in models.ProductType.objects.all().order_by('name'):
+        for product_type in models.ProductType.objects.all().filter(show_in_menu=True).order_by('name')[:10]:
             product_types_infos.append((product_type, reverse('shop_product_type_info', kwargs={"pk": product_type.pk})))
         cache.set('shop_product_type_infos', product_types_infos, 3600)
 
@@ -22,7 +22,7 @@ def get_shop_designs():
     designs_infos = cache.get('shop_design_infos', [])
 
     if not designs_infos:
-        for design in models.Design.objects.all().order_by('name'):
+        for design in models.Design.objects.all().filter(show_in_menu=True).order_by('name')[:10]:
             designs_infos.append((design, reverse('shop_design_info', kwargs={"pk": design.pk})))
 
         cache.set('shop_design_infos', designs_infos, 3600)
