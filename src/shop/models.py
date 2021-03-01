ce3f7be5c8c9faa185e1bc2d6f6151ea298defde
Arthur Jovart <arthur@jovart.com>
2021-03-01 17:52:47 +0100

    Add show in menu boolean

--- src/shop/models.py
+++ src/shop/models.py
@@ -9,6 +9,7 @@ from django.db import models
 
 class Design(models.Model):
     name = models.CharField(max_length=300, unique=True)
+    show_in_menu = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -16,6 +17,7 @@ class Design(models.Model):
 
 class ProductType(models.Model):
     name = models.CharField(max_length=300, unique=True)
+    show_in_menu = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
