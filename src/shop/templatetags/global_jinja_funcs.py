aee14b3023fecf3a653f14a0f94c629be65c53ac
Arthur Jovart <arthur@jovart.com>
2021-03-01 17:10:54 +0100

    Update get_shop_designs and products types to use cache.

--- src/shop/templatetags/global_jinja_funcs.py
+++ src/shop/templatetags/global_jinja_funcs.py
@@ -2,21 +2,29 @@ from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django_jinja import library
 from .. import models
-
-product_types_infos = []
-for product_type in models.ProductType.objects.all().order_by('name'):
-    product_types_infos.append((product_type, reverse('shop_product_type_info', {"pk": product_type.pk})))
-
-designs_infos = []
-for design in models.Design.objects.all().order_by('name'):
-    designs_infos.append((design, reverse('shop_design_info', {"pk": design.pk})))
+from django.core.cache import cache
 
 
 @library.global_function()
 def get_shop_product_types():
+    product_types_infos = cache.get('shop_product_type_infos')
+
+    if not product_types_infos:
+        for product_type in models.ProductType.objects.all().order_by('name'):
+            product_types_infos.append((product_type, reverse('shop_product_type_info', kwargs={"pk": product_type.pk})))
+        cache.set('shop_product_type_infos', product_types_infos, 3600)
+
     return product_types_infos
 
 
 @library.global_function()
 def get_shop_designs():
+    designs_infos = cache.get('shop_design_infos')
+
+    if not designs_infos:
+        for design in models.Design.objects.all().order_by('name'):
+            designs_infos.append((design, reverse('shop_design_info', kwargs={"pk": design.pk})))
+
+        cache.set('shop_design_infos', designs_infos, 3600)
+
     return designs_infos
