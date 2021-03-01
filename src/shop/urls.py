77221e3b98719e55d8d1b863d8388347d41a546c
Arthur Jovart <arthur@jovart.com>
2021-03-01 17:53:27 +0100

    Add Menu for the shop

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="shop_index"),
-    path('product/<int:product_id>', views.view_product, name="shop_product_info"),
-    path('product/<int:product_id>/info', views.product_api, name="shop_product_api"),
-    path('design/<int:pk>', views.view_design, name="shop_design_info"),
-    path('product_type/<int:pk>', views.view_product_type, name="shop_product_type_info"),
+    path('products/<int:product_id>', views.view_product, name="shop_product_info"),
+    path('products/<int:product_id>/info', views.product_api, name="shop_product_api"),
+    path('designs/<int:pk>', views.view_design, name="shop_design_info"),
+    path('product_types/<int:pk>', views.view_product_type, name="shop_product_type_info"),
+    path('designs', views.index, name="shop_designs"),
+    path('product_types', views.view_product_type, name="shop_product_types"),
 ]
