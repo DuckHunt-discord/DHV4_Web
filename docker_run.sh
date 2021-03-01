ea5b99f07ec8fcd86f64a571ad4f3e0ccd37267a
Arthur Jovart <arthur@jovart.com>
2021-03-01 17:03:10 +0100

    Fix run script to stop running check before migrating.

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic --clear --no-input
-python manage.py migrate --no-input
+python manage.py migrate --no-input --skip-checks
 
 exec gunicorn DHV4_Web.wsgi:application --access-logfile '-' --error-logfile '-' --log-level "debug" --capture-output --bind 0.0.0.0:8080 --workers=4
