e8a50ba46be2159b77b761c2480b09289f05cb3a
Arthur Jovart <arthur@jovart.com>
2021-03-09 18:04:41 +0100

    Fix performance on product types ?

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="shop_index"),
+    path('products/', views.view_product, name="shop_product_info_without_ID"),
     path('products/<int:product_id>', views.view_product, name="shop_product_info"),
     path('products/<int:product_id>/info', views.product_api, name="shop_product_api"),
     path('designs/<int:pk>', views.view_design, name="shop_design_info"),
