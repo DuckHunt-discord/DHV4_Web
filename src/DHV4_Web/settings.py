bbb09b3203ae169ad8fee77f347b0a9392a2c3f5
Arthur Jovart <arthur@jovart.com>
2021-03-29 22:41:10 +0200

    Automatic translation of interfaces.

--- src/DHV4_Web/settings.py
+++ src/DHV4_Web/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'pipeline.middleware.MinifyHTMLMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
