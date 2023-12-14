31d756df987bbe7320f48211fb2bbb6a6a229789
Arthur <site-github@api-d.com>
2023-12-14 13:26:44 +0100

    Fuuu

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,4 @@ git pull --recurse-submodules
 python manage.py collectstatic --clear --no-input
 python manage.py migrate --no-input --skip-checks
 
-exec gunicorn DHV4_Web.wsgi:application ----access-logfile None --error-logfile '-' --log-level "debug" --capture-output --bind 0.0.0.0:8080 --workers=4
+exec gunicorn DHV4_Web.wsgi:application --access-logfile None --error-logfile '-' --log-level "debug" --capture-output --bind 0.0.0.0:8080 --workers=4
